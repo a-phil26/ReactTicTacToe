11a1d66f07f41f03cdf4b4d9351df7cd6ed75ede
Addison Phillips <85582850+a-phil26@users.noreply.github.com>
2024-01-26 15:23:10 -0500

    Added the history tracking and displaying the history as a list of buttons to later return back to

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -55,25 +55,44 @@ function Board({xIsNext, squares, onPlay}) {
   );
 }
 
-export default function Game(){
+export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length-1];
+  const currentSquares = history[history.length - 1];
 
-  function handlePlay(nextSquares){
-
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
-  return(
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol></ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
-
 }
 
 function calculateWinner(squares) {
